504e29204c3a1a5ffa2457d5eec294f717e34c6d
Santosh kumar payasi <102188325+SantoshPayasi@users.noreply.github.com>
2023-10-21 20:37:28 +0530

    Changes in gitignore file

--- blogsource/src/App.tsx
+++ blogsource/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import PageRenderer from './PageRenderer'
+import Navbar from './components/global/Navbar'
 const App = () => {
   return (
     <div>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path= "/" Component={PageRenderer} />
           <Route path= "/:page" Component={PageRenderer} />
